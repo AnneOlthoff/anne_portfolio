8c20f1c452c408b79c223a5436e00a0cb7ecd68d
anne1996@live.se <anne1996@live.se>
2025-10-23 18:47:37 +0200

    fix nix?

--- flake.nix
+++ flake.nix
@@ -38,7 +38,7 @@
           # see https://github.com/NixOS/nixpkgs/blob/master/pkgs/by-name/cr/crabfit-frontend/package.nix
         preBuild = ''
           cp "${
-            google-fonts.override { fonts = [ "Inter" ]; }
+            nixpkgs.google-fonts.override { fonts = [ "Inter" ]; }
           }/share/fonts/truetype/Inter[slnt,wght].ttf" src/app/Inter.ttf
         '';
         postInstall = ''
